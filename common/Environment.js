b83b5934e1c8c232420991585cdc5d48f19e1104
shreyassali <salishreyas@gmail.com>
2017-12-24 18:02:01 -0800

    Added firebase variables

---
+++ common/Environment.js
@@ -0,0 +1,16 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPKnrvHFlQeHuaRX_jmFWhu9ScZ4sYL04",
+  authDomain: "pmpguide-b8d72.firebaseapp.com",
+  databaseURL: "https://pmpguide-b8d72.firebaseio.com",
+  projectId: "pmpguide-b8d72",
+  storageBucket: "pmpguide-b8d72.appspot.com",
+  messagingSenderId: "323096544554"
+};
+
+const Environment = {
+  logLevel: "DEBUG", //RAW, DEBUG, INFO
+  firebaseConfig: firebaseConfig,
+};
+
+module.exports = Environment;
