db5dd5f2920bf82f83530c633a51101e6b0fee94
shreyassali <salishreyas@gmail.com>
2017-12-24 18:38:23 -0800

    Added logger file for logging

---
+++ common/Logger.js
@@ -0,0 +1,40 @@
+const Environment = require('./Environment');
+const logPrefix = "PMP-GUIDE-LOG-";
+
+/**
+ * Log levels supported :
+ * RAW - Verbose log level is for logging raw wire logs like service request/response
+ * ERROR - Error log level is for logging application exceptions or errors
+ * INFO - Info log level is for logging informational logs
+ * DEBUG - Debug log level is for logging debugging statements
+ **/
+function log(level, message) {
+  if (Environment.logLevel == "RAW") {
+    console.log(logPrefix + level +  " : " + message);
+  } else if (Environment.logLevel == "ERROR" && level == "ERROR") {
+    console.log(logPrefix + level +  " : " + message);
+    Sentry.captureMessage(message);
+  } else if (Environment.logLevel == "INFO" && level == "INFO") {
+    console.log(logPrefix + level +  " : " + message);
+  } else if (Environment.logLevel == "DEBUG" && (level == "DEBUG" || level == "INFO")) {
+    console.log(logPrefix + level +  " : " + message);
+  }
+}
+
+function raw(message) {
+  log("RAW", message);
+}
+
+function error(message) {
+  log("ERROR", message);
+}
+
+function debug(message) {
+  log("DEBUG", message);
+}
+
+function info(message) {
+  log("INFO", message);
+}
+
+module.exports = { log, raw, error, debug, info };
