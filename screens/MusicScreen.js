2d379e93103baee2a08fd1fd053ffcfda980c14e
shreyassali <salishreyas@gmail.com>
2017-12-26 13:10:21 -0800

    Added Music card to music screen

--- screens/MusicScreen.js
+++ screens/MusicScreen.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Dimensions, View } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+
+import MusicCard from '../components/MusicCard';
+
 
 import firebase from 'firebase';
 import Banner from '../components/Banner';
 
 const { width } = Dimensions.get('window');
 
-export default class LinksScreen extends React.Component {
+export default class MusicScreen extends React.Component {
   state = {
     isLoadingComplete: false,
     audioList: null
   };
 
-  static navigationOptions = {
+  static navigationOptions = ({ navigation, screenProps }) => ({
     header:(
       <Banner headerText={'Select Your Music'}/>
     ),
-  };
+  });
+
   componentDidMount() {
  		setTimeout(() => {this.scrollView.scrollTo({x: -30}) }, 1) // scroll view position fix
     if(this.state.isLoadingComplete) {
       return;
     }
-    console.log("Music file");
     //Read from firebase
     var audioList = [];
     firebase.database().ref('audioList').once('value').then(function(snapshot) {
       snapshot.forEach(function(childSnapshot) {
-        console.log("Music file 2");
         var audio = childSnapshot.val();
         audio.id = childSnapshot.key;
         console.log(audio);
@@ -38,6 +39,13 @@ export default class LinksScreen extends React.Component {
     }).then((audioList) => this.setState({isLoadingComplete: true,
                   audioList: audioList}));
  	}
+
+  _renderItem = ({item}) => (
+    <ChapterCard
+      id={item.id}
+      name={item.name}
+      onPress={() => this._onPressItem(item.id)} />
+  );
 
   render() {
     return (
@@ -50,12 +58,8 @@ export default class LinksScreen extends React.Component {
         snapToInterval={width - 60}
         snapToAlignment={"center"}
         contentInset={{ top: 0, left: 30, bottom: 0, right: 30, }}>
-
-        <View style={styles.view} />
-        <View style={styles.view2} />
-        <View style={styles.view} />
-        <View style={styles.view2} />
-        <View style={styles.view} />
+        
+        <MusicCard/>
 
       </ScrollView>
     );
